dfee16730a7a8cd6caafa91ae30e5e91d194ad1e
Flowrey <flowrey@laposte.net>
2023-06-17 23:42:22 +0200

    fix: use a debian archive url

--- oxide-core/src/lib.rs
+++ oxide-core/src/lib.rs
@@ -7,7 +7,7 @@ mod tests {
     use super::*;
     use hex_literal::hex;
 
-    const DEBIAN_URL: &'static str = "https://cdimage.debian.org/debian-cd/current/amd64/bt-cd/debian-12.0.0-amd64-netinst.iso.torrent";
+    const DEBIAN_URL: &'static str = "https://cdimage.debian.org/cdimage/archive/11.7.0/amd64/bt-cd/debian-11.7.0-amd64-netinst.iso.torrent";
 
     #[test]
     fn parse_torrent_file() {
@@ -18,12 +18,12 @@ mod tests {
             metainfo.announce(),
             "http://bttracker.debian.org:6969/announce"
         );
-        assert_eq!(info.name(), "debian-12.0.0-amd64-netinst.iso");
-        assert_eq!(info.length(), Some(773849088));
+        assert_eq!(info.name(), "debian-11.7.0-amd64-netinst.iso");
+        assert_eq!(info.length(), Some(407896064));
         assert_eq!(info.piece_length(), 262144);
         assert_eq!(
             info.hash()[..],
-            hex!("b851474b74f65cd19f981c723590e3e520242b97")
+            hex!("6f84758b0ddd8dc05840bf932a77935d8b5b8b93")
         );
     }
 }
